8253be004e05df6a30d501b72ef19bd8571f8502
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-07-23 09:46:36 +0900

    add support for digest authentication

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+PASSWD_FILE=$PWD/conf/passwordfile
+
+if [ ! -e "$PASSWD_FILE" ]; then
+    echo "Please create password file ($PASSWD_FILE) first."
+    exit 1
+fi
+
 docker run -it -p 8080:80 \
+    -v "$PASSWD_FILE":/usr/local/apache2/conf/passwordfile \
     -v "$PWD/conf/httpd.conf":/usr/local/apache2/conf/httpd.conf \
     -v "$PWD/public-html":/usr/local/apache2/htdocs/ \
     httpd:2.4
