bd60db5c5d7466aba202fe5112709cf9a9fd4384
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-07-23 09:47:08 +0900

    add parameter

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ docker run -it -p 8080:80 \
     -v "$PASSWD_FILE":/usr/local/apache2/conf/passwordfile \
     -v "$PWD/conf/httpd.conf":/usr/local/apache2/conf/httpd.conf \
     -v "$PWD/public-html":/usr/local/apache2/htdocs/ \
-    httpd:2.4
+    httpd:2.4 $1
